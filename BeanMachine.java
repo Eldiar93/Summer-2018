b9588deb435f46dd30a0b2ceb6e365745e8cb989
Bryce James Rowley <40773615+Eldiar93@users.noreply.github.com>
2018-08-30 12:29:48 -0700

    Add files via upload

---
+++ BeanMachine.java
@@ -0,0 +1,19 @@
+import java.util.*
+//Author: Bryce Rowley
+//Summer 2018 Final
+//This program creates a Galton Box
+
+public class BeanMachine {
+
+    public static final int NUM_SLOTS = 8;
+    
+        
+
+
+//Array of tallies
+int[] counts = new int[10];
+while (n > 0) {
+    counts[n % 10]++;
+    n = n/10;
+    
+}
